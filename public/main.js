bc49452542935faf5d96d6195125ffae9e2fe014
Petr Broz <petr.broz@autodesk.com>
2022-03-07 10:11:18 +0100

    Added proper logout method.

--- public/main.js
+++ public/main.js
@@ -7,7 +7,17 @@ try {
     if (resp.ok) {
         const user = await resp.json();
         login.innerText = `Logout (${user.name})`;
-        login.onclick = () => window.location.replace('/api/auth/logout');
+        login.onclick = () => {
+            // Log the user out (see https://forge.autodesk.com/blog/log-out-forge)
+            const iframe = document.createElement('iframe');
+            iframe.style.visibility = 'hidden';
+            iframe.src = 'https://accounts.autodesk.com/Authentication/LogOut';
+            document.body.appendChild(iframe);
+            iframe.onload = () => {
+                window.location.replace('/api/auth/logout');
+                document.body.removeChild(iframe);
+            };
+        }
         const viewer = await initViewer(document.getElementById('preview'));
         initTree('#tree', (id) => loadModel(viewer, window.btoa(id).replace(/=/g, '')));
     } else {
