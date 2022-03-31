d562dde22911ccb24106e95736a95815146c7289
Petr Broz <petr.broz@autodesk.com>
2022-03-31 13:59:12 +0200

    Added dashboard extensions.

--- wwwroot/viewer.js
+++ wwwroot/viewer.js
@@ -1,22 +1,37 @@
 /// import * as Autodesk from "@types/forge-viewer";
 
+import './extensions/LoggerExtension.js';
+import './extensions/SummaryExtension.js';
+import './extensions/DataGridExtension.js';
+import './extensions/HistogramExtension.js';
+
 async function getAccessToken(callback) {
     try {
         const resp = await fetch('/api/auth/token');
-        if (!resp.ok)
+        if (!resp.ok) {
             throw new Error(await resp.text());
+        }
         const { access_token, expires_in } = await resp.json();
         callback(access_token, expires_in);
     } catch (err) {
         alert('Could not obtain access token. See the console for more details.');
-        console.error(err);        
+        console.error(err);
     }
 }
 
 export function initViewer(container) {
     return new Promise(function (resolve, reject) {
-        Autodesk.Viewing.Initializer({ getAccessToken }, async function () {
-            const viewer = new Autodesk.Viewing.GuiViewer3D(container);
+        Autodesk.Viewing.Initializer({ getAccessToken }, function () {
+            const config = {
+                extensions: [
+                    'Autodesk.DocumentBrowser',
+                    'LoggerExtension',
+                    'SummaryExtension',
+                    'DataGridExtension',
+                    'HistogramExtension'
+                ]
+            };
+            const viewer = new Autodesk.Viewing.GuiViewer3D(container, config);
             viewer.start();
             viewer.setTheme('light-theme');
             resolve(viewer);
@@ -25,12 +40,14 @@ export function initViewer(container) {
 }
 
 export function loadModel(viewer, urn) {
-    function onDocumentLoadSuccess(doc) {
-        viewer.loadDocumentNode(doc, doc.getRoot().getDefaultGeometry());
-    }
-    function onDocumentLoadFailure(code, message) {
-        alert('Could not load model. See console for more details.');
-        console.error(message);
-    }
-    Autodesk.Viewing.Document.load('urn:' + urn, onDocumentLoadSuccess, onDocumentLoadFailure);
+    return new Promise(function (resolve, reject) {
+        function onDocumentLoadSuccess(doc) {
+            resolve(viewer.loadDocumentNode(doc, doc.getRoot().getDefaultGeometry()));
+        }
+        function onDocumentLoadFailure(code, message, errors) {
+            reject({ code, message, errors });
+        }
+        viewer.setLightPreset(0);
+        Autodesk.Viewing.Document.load('urn:' + urn, onDocumentLoadSuccess, onDocumentLoadFailure);
+    });
 }
